53255beae52da1f955a8f0b4b2a5ea47d53db722
Keenan Mendiola <keenanpaul980@gmail.com>
2019-03-07 04:24:03 +0800

    Implemented Redux as central store

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const GET_INCIDENTS = 'GET_INCIDENTS';
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import incidentsReducer from './incidentsReducer';
+
+const rootReducer = combineReducers({
+    incidents: incidentsReducer
+}); 
+
+export default rootReducer;
